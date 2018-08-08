476e1786da46738a66b88d58ff3169015f54a560
Salih KARAHAN <salihkarahan@outlook.com>
2018-08-09 01:33:39 +0300

    drawing işlemleri farklı bir js'e çekildi.

--- src/drawing.js
+++ src/drawing.js
@@ -1,8 +1,37 @@
 jQuery(document).ready(function() {
 
-    var map = new ol.Map({ target: 'map' });
+    debugger;
+    var map = $('#map').data('map');
 
+    var drawingSource = new ol.source.Vector({
+        wrapX: false
+    });
 
+    var drawingLayer = new ol.layer.Vector({
+        source: drawingSource
+    });
 
+    var typeSelect = document.getElementById('type');
+
+    var draw;
+    var addInteraction = function() {
+        var value = typeSelect.value;
+        if (value !== 'None') {
+            draw = new ol.interaction.Draw({
+                source: drawingSource,
+                type: typeSelect.value
+            });
+
+            map.addInteraction(draw);
+        }
+    }
+
+    typeSelect.onchange = function() {
+        map.removeInteraction(draw);
+        addInteraction();
+    }
+
+    map.addLayer(drawingLayer);
+    addInteraction();
 
 });
